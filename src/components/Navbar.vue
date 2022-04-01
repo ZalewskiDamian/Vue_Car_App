a1ea3a8252218c0c017677233771f8576c503a89
ZalewskiDamian <damian.github@gmail.com>
2022-04-01 14:33:13 +0200

    add search input

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -25,8 +25,6 @@
 
 <script>
 
-// import { computed, onMounted } from "vue";
-
 export default {
     name: "Navbar",
     components: {
@@ -37,45 +35,3 @@ export default {
     },
 };
 </script>
-
-<style scoped>
-.navbar {
-    width: 100%;
-    background-color: #8D3131;
-    padding: 0 20px;
-}
-.navbar__wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-.navbar__nav {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-}
-.navbar__navItem:hover {
-    background-color: #6c0e0e;
-}
-.navbar__link {
-    display: block;
-    color: white;
-    font-size: 18px;
-    padding: 30px 20px;
-}
-.navbar__btn {
-    display: block;
-    color: black;
-    background-color: white;
-    padding: 10px 15px;
-    font-weight: 500;
-    border-radius: 4px;
-    flex-shrink: 0;
-    transition: all .2s ease-in-out;
-}
-.navbar__btn:hover {
-    background-color: black;
-    color: white;
-}
-</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,22 +1,33 @@
 <template>
     <section class="home">
-       tresc home <span class="test">{{sections}}</span>
+       <div class="container">
+           <h1 class="section-heading">Nasze samochody</h1>
+           <div class="search">
+               <input type="search" class="search__input" placeholder="Szukaj...">
+           </div>
+           <div class="filters">
+               <v-select :options="['Canada', 'United States']"></v-select>
+           </div>
+       </div>
     </section>
 </template>
 
 <script>
+import Vue from "vue";
 import { useStore } from "vuex";
+import vSelect from "vue-select-3";
 // import { computed, onMounted } from "vue";
 
 export default {
     components: {
+        vSelect
     },
     mixins: [],
     setup() {
         const store = useStore();
         console.log(store);
-        const sections = 'test';
-        return { sections };
+        // const sections = 'test';
+        // return { sections };
     },
 };
 </script>
