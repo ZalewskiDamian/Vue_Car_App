ba4cb604d2bfda37569204834c66101bb2ef1b62
ZalewskiDamian <damian.github@gmail.com>
2022-04-01 16:25:15 +0200

    add filters wrapper

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,14 +6,70 @@
                <input type="search" class="search__input" placeholder="Szukaj...">
            </div>
            <div class="filters">
-               <vue-select searchable :options="[{ color: 'Red' }, { color: 'Green' }]" label-by="color"> </vue-select>
+                <div class="filters__inner">
+                    <p class="filters__title">Rok produkcji</p>
+                    <vue-select
+                        class="filters__select"
+                        searchable 
+                        :options="[{ color: 'Red' }, { color: 'Green' }]" 
+                        label-by="color"
+                        clear-on-close
+                        search-placeholder="Test"
+                    /> 
+               </div>
+                <div class="filters__inner">
+                    <p class="filters__title">Pojemność</p>
+                    <vue-select
+                        class="filters__select"
+                        searchable 
+                        :options="[{ color: 'Red' }, { color: 'Green' }]" 
+                        label-by="color"
+                        clear-on-close
+                        search-placeholder="Select user"
+                    /> 
+               </div>
+                <div class="filters__inner">
+                    <p class="filters__title">Paliwo</p>
+                    <vue-select
+                        class="filters__select"
+                        searchable 
+                        :options="[{ color: 'Red' }, { color: 'Green' }]" 
+                        label-by="color"
+                        clear-on-close
+                        search-placeholder="Select user"
+                    /> 
+               </div>
+                <div class="filters__inner">
+                    <p class="filters__title">Kolor</p>
+                    <vue-select
+                        class="filters__select"
+                        searchable 
+                        :options="[{ color: 'Red' }, { color: 'Green' }]" 
+                        label-by="color"
+                        clear-on-close
+                        search-placeholder="Select user"
+                    /> 
+               </div>
+                <div class="filters__inner">
+                    <p class="filters__title">Karoseria</p>
+                    <vue-select
+                        class="filters__select"
+                        searchable 
+                        :options="[{ color: 'Red' }, { color: 'Green' }]" 
+                        label-by="color"
+                        clear-on-close
+                        search-placeholder="Select user"
+                    /> 
+               </div>
+               <div class="filters__buttonWrapper">
+                   <button type="button" class="filters__searchBtn">Szukaj</button>
+               </div>
            </div>
        </div>
     </section>
 </template>
 
 <script>
-import Vue from "vue";
 import { useStore } from "vuex";
 import VueNextSelect from 'vue-next-select'
 import "vue-next-select/dist/index.css";
