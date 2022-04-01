b27ee87b8d222357d77ce6a4f8a21381c8910675
ZalewskiDamian <damian.github@gmail.com>
2022-04-01 11:49:19 +0200

    add navi component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,24 @@
 <template>
     <div class="navbar">
        <div class="navbar__wrapper">
-           <nav class="navbar__navigation">Nav</nav>
+           <ul class="navbar__nav">
+               <li class="navbar__navItem">
+                   <a href="#" class="navbar__link">Samochody</a>
+               </li>
+               <li class="navbar__navItem">
+                   <a href="#" class="navbar__link">Serwis</a>
+               </li>
+               <li class="navbar__navItem">
+                   <a href="#" class="navbar__link">LOGO</a>
+               </li>
+               <li class="navbar__navItem">
+                   <a href="#" class="navbar__link">Kontakt</a>
+               </li>
+               <li class="navbar__navItem">
+                   <a href="#" class="navbar__link">O nas</a>
+               </li>
+           </ul>
+            <a href="#" class="navbar__btn">Zaloguj się</a>
        </div>
     </div>
 </template>
@@ -24,6 +41,41 @@ export default {
 <style scoped>
 .navbar {
     width: 100%;
-    background-color: blue;
+    background-color: #8D3131;
+    padding: 0 20px;
+}
+.navbar__wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.navbar__nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
+.navbar__navItem:hover {
+    background-color: #6c0e0e;
+}
+.navbar__link {
+    display: block;
+    color: white;
+    font-size: 18px;
+    padding: 30px 20px;
+}
+.navbar__btn {
+    display: block;
+    color: black;
+    background-color: white;
+    padding: 10px 15px;
+    font-weight: 500;
+    border-radius: 4px;
+    flex-shrink: 0;
+    transition: all .2s ease-in-out;
+}
+.navbar__btn:hover {
+    background-color: black;
+    color: white;
 }
 </style>
