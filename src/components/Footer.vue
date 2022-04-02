a77504c429d97c1d419e05cec9e344cdc070328b
ZalewskiDamian <damian.github@gmail.com>
2022-04-02 18:21:40 +0200

    update footer styles

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,24 @@
 <template>
     <footer class="footer">
-        Footer
+        <div class="footer__content">
+            <p class="footer__heading">
+                CLASSIC CARS GRANADA 2016
+            </p>
+            <div class="footer__iconsWrapper">
+                <a href="#" class="footer__link">
+                    <img src="../assets/images/twitter-icon.png" alt="twitter" class="footer__icon">
+                </a>
+                <a href="#" class="footer__link">
+                    <img src="../assets/images/facebook-icon.png" alt="facebook" class="footer__icon">
+                </a>
+                <a href="#" class="footer__link">
+                    <img src="../assets/images/instagram-icon.png" alt="instagram" class="footer__icon">
+                </a>
+                <a href="#" class="footer__link">
+                    <img src="../assets/images/youtube-icon.png" alt="youtube" class="footer__icon">
+                </a>
+            </div>
+        </div>
     </footer>
 </template>
 
